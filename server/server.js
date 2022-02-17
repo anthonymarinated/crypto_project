d9791969e705a82392817af8c7c8035126a03227
anthonymarinated <anthonymarin718@gmail.com>
2022-02-17 18:18:20 -0500

    allfiles

---
+++ server/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const axios = require('axios');
+//node ./server/server.js
+// statically serve everything in the build folder on the route '/build'
+app.use('/build', express.static(path.join(__dirname, '../build')));
+
+app.get('/api/coins', (req, response) => {
+  axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?limit=10&sort=cmc_rank', {
+      headers: {
+        'X-CMC_PRO_API_KEY': '00621e99-a961-4bae-af28-e4d6afa0b1aa',
+      },
+    })
+  .then(({data}) => {
+  console.log(data);
+  response.json(data);
+  });
+})
+app.get('/api/top20', (req, response) => {
+  axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?limit=20&sort=cmc_rank', {
+      headers: {
+        'X-CMC_PRO_API_KEY': '00621e99-a961-4bae-af28-e4d6afa0b1aa',
+      },
+    })
+  .then(({data}) => {
+  response.json(data);
+  });
+})
+app.get('/api/top100', (req, response) => {
+  axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?limit=100&sort=cmc_rank', {
+      headers: {
+        'X-CMC_PRO_API_KEY': '00621e99-a961-4bae-af28-e4d6afa0b1aa',
+      },
+    })
+  .then(({data}) => {
+  response.json(data);
+  });
+})
+app.get('/api/inactivecoins', (req, response) => {
+  axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?limit=30&listing_status=inactive', {
+      headers: {
+        'X-CMC_PRO_API_KEY': '00621e99-a961-4bae-af28-e4d6afa0b1aa',
+      },
+    })
+  .then(({data}) => {
+  response.json(data);
+  });
+})
+// serve index.html on the route '/'
+app.get('/', (req, res) => {
+  return res.status(200).sendFile(path.join(__dirname, '../index.html'));
+});
+
+app.listen(3000); //listens on port 3000 -> http://localhost:3000/
+
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    //change to development so page reloads everytime there is a change
+    mode: 'development',
+    entry: [
+        //entry point of app
+        './client/index.js'
+    ],
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/',
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+            template: 'index.html'
+        }),
+    ],
+    devServer: {
+        hot: true,
+        historyApiFallback: true,
+        static: {
+            directory: path.resolve(__dirname, 'build'),
+            publicPath: '/build',
+        },
+        proxy: {
+          '/api': 'http://localhost:3000'
+        }
+    },
+    module: {
+        rules: [
+          {
+            // /\.jsx?/,
+            test: /.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react']
+              }
+            },
+          },
+          {
+            test: /.(css|scss)$/, // /\.s?css/,  
+            exclude: /node_modules/,
+            use: [
+              'style-loader',
+              'css-loader',
+              'sass-loader'
+            ]
+          },
+        ]
+      },
+}
